be96ecb07726a301402aa909009ed11c95f32bcc
william-blue-ant <william.manirakiza14@gmail.com>
2025-11-27 09:47:57 -0500

    continued first tutorial; made tabs (2/9)

---
+++ StickerTest/app/+not-found.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Link, Stack } from 'expo-router';
+
+export default function Index() {
+
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Oops! Not Found' }}/>
+      <View style={styles.container}>
+      <Link href="./" style={styles.button}>
+          Go to Home screen!
+      </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+})
