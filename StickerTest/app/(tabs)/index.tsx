8762c8c347d6df0ebb1bd96ec5e1292e302616bb
william-blue-ant <william.manirakiza14@gmail.com>
2025-11-28 13:58:18 -0500

    finished first screen (Expo tutorial 3/9)

--- StickerTest/app/(tabs)/index.tsx
+++ StickerTest/app/(tabs)/index.tsx
@@ -1,14 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+
+const PlaceHolderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="./about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceHolderImage} />
+      </View>
+      <View>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -18,14 +25,12 @@ const styles = StyleSheet.create({
     flex:1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: {
-    color: '#fff',
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
-})
+});
