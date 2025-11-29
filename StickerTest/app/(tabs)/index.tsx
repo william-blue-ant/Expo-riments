f72a76c7188a79a1079bd84000dbd03eaa4fb273
william-blue-ant <william.manirakiza14@gmail.com>
2025-11-29 13:25:42 -0500

    added gestures (Expo tutorial 6/9)

--- StickerTest/app/(tabs)/index.tsx
+++ StickerTest/app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, View, ImageSourcePropType } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
@@ -53,7 +54,7 @@ export default function Index() {
 
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer defaultImage={PlaceHolderImage} selectedImage={selectedImage} />
         {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
@@ -75,7 +76,7 @@ export default function Index() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
